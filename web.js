6d93275706576de6986b05791884f77a9afd1b7b
Josh Zimmer <zimphys@gmail.com>
2013-07-06 13:15:10 +0000

    Reads index.html from web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html').toString());
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
